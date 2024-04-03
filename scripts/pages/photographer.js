163f2c2a15142496de671f95dc229a8cecd8388f
Flora Paul <flora.p3108@gmail.com>
2024-04-03 09:50:41 +0200

    dropdow JS

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,2 +1,14 @@
 //Mettre le code JavaScript lié à la page photographer.html
  
+//on met un eventment au click pour trié en fonction de la popularité, de la date et du titre
+const $trie = document.getElementById("trie");
+const liste = document.querySelector(".dropdown-options");
+
+$trie.addEventListener('click', () => {
+    liste.style.display = "block";
+});
+
+const $close = document.getElementById("popu");
+$close.addEventListener('click', () =>{
+    liste.style.display = "none";
+})
